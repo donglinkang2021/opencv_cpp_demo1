4a04ab7243dac015d45df89e03daa17d0a8d9e38
Linkdom <donglinkang2021@163.com>
2024-04-05 15:46:54 +0800

    finish pic concat dev ver 0.1 with more concise function

--- opencv_cpp_demo1/concatSubtitle.cpp
+++ opencv_cpp_demo1/concatSubtitle.cpp
@@ -4,12 +4,18 @@ double calculateImageDifference(const cv::Mat& gray_image1, const cv::Mat& gray_
     cv::Mat diff;
     cv::absdiff(gray_image1, gray_image2, diff);
     cv::Scalar diff_sum = cv::sum(diff);
+    
+    double diff_sum_total = 0;
+    size_t num_channels = gray_image1.channels();
+    for (size_t i = 0; i < num_channels; ++i) diff_sum_total += diff_sum[i];
+
     double num_pixels = gray_image1.rows * gray_image1.cols;
-    return diff_sum[0] / num_pixels;
+    return diff_sum_total / (num_pixels * num_channels);
 }
 
 void concatenateSimilarFrames(const std::vector<cv::Mat>& frames) {
     if (frames.empty()) return;
+
     cv::Mat concatenated_image = frames[0].clone();
     int frame_width = frames[0].cols;
     int frame_half_width = frame_width / 2;
@@ -25,7 +31,6 @@ void concatenateSimilarFrames(const std::vector<cv::Mat>& frames) {
             if (i + j >= frames.size()) break;
             cv::Mat cur_frame_left = frames[i + j](roi_left).clone();
             double diff = calculateImageDifference(cur_template, cur_frame_left);
-            std::cout << "diff: " << diff << std::endl;
             if (diff < mindiff) {
                 mindiff = diff;
                 min_index = j;
@@ -33,15 +38,12 @@ void concatenateSimilarFrames(const std::vector<cv::Mat>& frames) {
         }
         if (min_index < 50) continue;
         i += min_index;
-        std::cout << "min_index: " << min_index << "min diff" << mindiff << std::endl;
         cv::Mat cur_frame_right = frames[i](roi_right).clone();
         cv::hconcat(concatenated_image, cur_frame_right, concatenated_image);
         cur_template = cur_frame_right.clone();
-        cv::imshow("cur template right", cur_template);
     }
 
 
-    std::cout << "-----Last frame:-----" << std::endl;
     // process final frame
     double mindiff = 10000;
     size_t min_x_index = 0;
@@ -51,13 +53,11 @@ void concatenateSimilarFrames(const std::vector<cv::Mat>& frames) {
         cv::Mat cur_frame = last_frame(roi).clone();
         double diff = calculateImageDifference(cur_template, cur_frame);
         if (diff < mindiff) {
-            std::cout << "diff: " << diff << std::endl;
-            std::cout << "min_x_index: " << min_x_index << std::endl;
             mindiff = diff;
             min_x_index = x;
         }
     }
-    size_t x = min_x_index + frame_half_width;
+    int x = min_x_index + frame_half_width;
     cv::Rect roi(x, 0, frame_width - x, frame_height);
     cv::Mat cur_frame_right = last_frame(roi).clone();
     cv::hconcat(concatenated_image, cur_frame_right, concatenated_image);
@@ -69,25 +69,11 @@ void concatenateSimilarFrames(const std::vector<cv::Mat>& frames) {
 
 int main(int argc, char* argv[]) {
 
-    cv::String pattern = "..\\..\\data\\Result\\Sample2\\*.jpg"; 
+    std::string data_path = "..\\..\\data\\Result\\Sample1\\";
+    cv::String pattern = data_path + "*.jpg";
 
     std::vector<cv::String> filenames;
     cv::glob(pattern, filenames, false);
-
-    // cv::Mat frame;
-    // int frame_freq = 60;
-    // int delay = 1000 / frame_freq;
-    // for (const auto& filename : filenames) {
-    //     frame = cv::imread(filename);
-    //     cv::imshow("subtitle frame", frame);
-    //     cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-    //     cv::imshow("subtitle frame_gray", frame);
-    //     // binary thresholding
-    //     cv::Mat binary_frame;
-    //     cv::threshold(frame, binary_frame, 127, 255, cv::THRESH_BINARY);
-    //     cv::imshow("subtitle frame_binary", binary_frame);
-    //     if (cv::waitKey(delay) == 27) break; // ESC key to quit
-    // }
 
     std::vector<cv::Mat> frames;
 
@@ -98,9 +84,10 @@ int main(int argc, char* argv[]) {
     for (const auto& filename : filenames) {
         frame = cv::imread(filename);
         cv::resize(frame, frame, cv::Size(frame_width, frame_height));
-        cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-        cv::threshold(frame, frame, 127, 255, cv::THRESH_BINARY);
-        cv::imshow("frame binary", frame);
+        // if you want your result to be in grayscale
+        // feel free to uncomment the following lines
+        // cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+        // cv::threshold(frame, frame, 127, 255, cv::THRESH_BINARY);
         if (!frame.empty()) frames.push_back(frame);        
     }
 
