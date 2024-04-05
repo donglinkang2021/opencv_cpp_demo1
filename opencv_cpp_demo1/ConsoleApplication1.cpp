2d0486a9aa6ef4ffea5fb056513a5d0c00aebde8
Linkdom <donglinkang2021@163.com>
2024-04-05 11:38:07 +0800

    play the sample1 and sample2 60 frame freq

--- opencv_cpp_demo1/ConsoleApplication1.cpp
+++ opencv_cpp_demo1/ConsoleApplication1.cpp
@@ -1,17 +1,19 @@
 ﻿#include <opencv2/opencv.hpp>  
 int main(int argc, char* argv[]) {
 
-    cv::String pattern = "../../data/Sample1/*.jpg"; 
+    cv::String pattern = "../../data/Sample2/*.jpg"; 
 
     // 读取文件夹中所有的图像帧
     std::vector<cv::String> filenames;
     cv::glob(pattern, filenames, false);
 
     cv::Mat frame;
+    int frame_freq = 60;
+    int delay = 1000 / frame_freq;
     for (const auto& filename : filenames) {
         frame = cv::imread(filename);
         cv::imshow("frame", frame);
-        cv::waitKey(0);
+        cv::waitKey(delay);
     }
     
     return 0;
